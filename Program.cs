309b646a8e5902c2c83604e5166f5ea3cef13f7c
KirsiL <117641781+Turvasumutin@users.noreply.github.com>
2023-01-24 14:29:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace sähköhintaprokkis;
+
+/*koodin pitää
+lukea pörssisähköhinta class hinta
+lukea sisälämpötila class lämpötila
+
+-> jos lämpötila alle 15c -> lämpötila pitää nostaa -> jos kallis sähkö -> ilmoita -> nappivaihtoehto takalle
+						    -> jos ok sähköhinta -> lämmitä normaali
+
+-> jos lämpötila yli 15c -> ok*/
+
+class Hinta
+{
+
+}
+
+class Lämpötila
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
